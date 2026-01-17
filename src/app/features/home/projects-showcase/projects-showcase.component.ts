dd66c9fa2f75157ce95437a5d82d35365d98ba2b
nourabuelenin <nourabuelenin@gmail.com>
2026-01-17 14:17:01 +0200

    padding fix

--- src/app/features/home/projects-showcase/projects-showcase.component.ts
+++ src/app/features/home/projects-showcase/projects-showcase.component.ts
@@ -29,7 +29,6 @@ import { TranslationService } from '../../../core/services/translation.service';
       text-align: center;
       max-width: 48rem;
       margin: 0 auto 3rem;
-      margin: 0 auto 3rem;
     }
 
     .section-tag {
