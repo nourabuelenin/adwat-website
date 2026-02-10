652f3bb57e0bf9f7762d2b63e95bebf2e4b8c0b4
nourabuelenin <nourabuelenin@gmail.com>
2026-02-10 20:24:53 +0200

    enviroment update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
-// Environment configuration for development
+// Environment configuration for production
 export const environment = {
   production: false,
   emailjs: {
     serviceId: '',
     templateId: '',
     publicKey: '',
+  },
+  microsoftClarity: {
+    projectId: '',
   },
 };
